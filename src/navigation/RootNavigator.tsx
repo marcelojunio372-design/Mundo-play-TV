ea125ef9eee1dc687bcb9acd9aad00028a8bcdf0
Marcelo <marcelojunio372@gmail.com>
2026-03-10 17:24:39 -0300

    restore real navigator

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SplashScreen from "../screens/SplashScreen";
+import LoginScreen from "../screens/LoginScreen";
+import HomeScreen from "../screens/HomeScreen";
+import BrowseScreen from "../screens/BrowseScreen";
+import PlayerScreen from "../screens/PlayerScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
   return (
-    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-      <Text>Mundo Play TV</Text>
-    </View>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Splash" component={SplashScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Browse" component={BrowseScreen} />
+      <Stack.Screen name="Player" component={PlayerScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
   );
 }
+
+
