de694b35361ce0ed710c550962ebd9860ef4747b
Marcelo <marcelojunio372@gmail.com>
2026-03-10 16:13:25 -0300

    add RootNavigator

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function RootNavigator() {
+  return (
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <Text>Mundo Play TV</Text>
+    </View>
+  );
+}
